045c588f7788b1b738be8fa4a79a760f77051cd3
GeekLad <geeklad@geeklad.com>
2024-07-11 16:03:45 -0400

    Added endpoints to fetch position data

--- services/MeteoraDlmmApi.ts
+++ services/MeteoraDlmmApi.ts
@@ -1,5 +1,6 @@
-import { cachedRequest, type UnifiedFetcher } from "./util";
-import * as _decimaljs2 from "decimal.js";
+import pThrottle from "p-throttle";
+
+import { cachedRequest, delay, type UnifiedFetcher } from "./util";
 
 const METEORA_API = "https://dlmm-api.meteora.ag";
 const METEORA_API_PAIR_ENDPOINT = "/pair/all";
@@ -33,6 +34,35 @@ export interface MeteoraDlmmPair {
   hide: boolean;
 }
 
+export interface MeteoraApidata {
+  tx_id: string;
+  onchain_timestamp: number;
+  position_address: string;
+  pair_address: string;
+}
+
+export interface MeteoraTransactionData extends MeteoraApidata {
+  active_bin_id: number;
+  price: number;
+  token_x_amount: number;
+  token_y_amount: number;
+  token_x_usd_amount: number;
+  token_y_usd_amount: number;
+}
+
+export interface MeteoraClaimFeesData extends MeteoraApidata {
+  token_x_amount: number;
+  token_x_usd_amount: number;
+  token_y_amount: number;
+  token_y_usd_amount: number;
+}
+
+export interface MeteoraClaimRewardsData extends MeteoraApidata {
+  reward_mint_address: string;
+  token_amount: number;
+  token_usd_amount: number;
+}
+
 export const getDlmmPairs = cachedRequest(
   async (fetcher: UnifiedFetcher = fetch) => {
     const pairResponse = await fetcher(METEORA_API + METEORA_API_PAIR_ENDPOINT);
@@ -40,6 +70,7 @@ export const getDlmmPairs = cachedRequest(
       await pairResponse.text(),
     ) as MeteoraDlmmPair[];
     const pairs: Map<string, MeteoraDlmmPair> = new Map();
+
     pairArray.forEach((pair) => pairs.set(pair.address, pair));
 
     return {
@@ -48,3 +79,52 @@ export const getDlmmPairs = cachedRequest(
     };
   },
 );
+
+export const THROTTLE_METEORA_DETAIL_API = pThrottle({
+  limit: 3,
+  interval: 1000,
+  strict: true,
+});
+
+const fetchMeteoraApiData = THROTTLE_METEORA_DETAIL_API(
+  async (
+    positionAddress: string,
+    endpoint: "/deposits" | "/withdraws" | "/claim_fees" | "/claim_rewards",
+    delayMs = 1000,
+  ): Promise<any> => {
+    const url = `${METEORA_API}/position/${positionAddress}${endpoint}`;
+    const response = await fetch(url);
+
+    if (response.status == 429) {
+      await delay(delayMs);
+
+      return fetchMeteoraApiData(positionAddress, endpoint, delayMs * 2);
+    }
+
+    return response.json();
+  },
+);
+
+export async function fetchDeposits(positionAddress: string) {
+  return fetchMeteoraApiData(positionAddress, "/deposits") as Promise<
+    MeteoraTransactionData[]
+  >;
+}
+
+export async function fetchWithdraws(positionAddress: string) {
+  return fetchMeteoraApiData(positionAddress, "/withdraws") as Promise<
+    MeteoraTransactionData[]
+  >;
+}
+
+export async function fetchFees(positionAddress: string) {
+  return fetchMeteoraApiData(positionAddress, "/claim_fees") as Promise<
+    MeteoraClaimFeesData[]
+  >;
+}
+
+export async function fetchRewards(positionAddress: string) {
+  return fetchMeteoraApiData(positionAddress, "/claim_rewards") as Promise<
+    MeteoraClaimRewardsData[]
+  >;
+}
