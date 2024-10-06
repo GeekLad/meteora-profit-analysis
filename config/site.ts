6d0f5413011e89c1f5f6dc3c465e7912c6e45505
GeekLad <geeklad@geeklad.com>
2024-10-06 15:49:39 -0400

    A few more callouts of v3.0

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Meteora DLMM Profit Analysis",
+  name: "Meteora DLMM Profit Analysis v3.0",
   description: "Find out how much you're printing",
 };
