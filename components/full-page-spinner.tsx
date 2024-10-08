cf7f5479e505347c1489505c563dbbb3eb5bd580
GeekLad <geeklad@geeklad.com>
2024-10-08 15:48:01 -0400

    Added excludeLayout flag

--- components/full-page-spinner.tsx
+++ components/full-page-spinner.tsx
@@ -2,7 +2,15 @@ import { Spinner } from "@nextui-org/react";
 
 import DefaultLayout from "@/layouts/default";
 
-export const FullPageSpinner = () => {
+export const FullPageSpinner = (props: { excludeLayout?: boolean }) => {
+  if (props.excludeLayout) {
+    return (
+      <div className="flex h-full w-full justify-center items-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
   return (
     <DefaultLayout>
       <div className="flex h-full w-full justify-center items-center">
