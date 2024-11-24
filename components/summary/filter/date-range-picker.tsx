0f4e0772d0fe8170a0639d837e88f6dcce280cb7
GeekLad <geeklad@geeklad.com>
2024-11-24 09:52:54 -0500

    Added buttons for today, last 7 days, last 30 days

--- components/summary/filter/date-range-picker.tsx
+++ components/summary/filter/date-range-picker.tsx
@@ -1,4 +1,9 @@
-import { DateRangePicker, RangeValue } from "@nextui-org/react";
+import {
+  Button,
+  ButtonGroup,
+  DateRangePicker,
+  RangeValue,
+} from "@nextui-org/react";
 import {
   CalendarDate,
   getLocalTimeZone,
@@ -16,6 +21,35 @@ export const PositionDateRangePicker = (props: {
     end: parseDate(props.end.toISOString().substring(0, 10)),
   } as RangeValue<CalendarDate>;
 
+  const today = {
+    start: parseDate(new Date(Date.now()).toISOString().substring(0, 10)),
+    end: parseDate(
+      new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString().substring(0, 10),
+    ),
+  };
+
+  const last7Days = {
+    start: parseDate(
+      new Date(Date.now() - 1000 * 60 * 60 * 24 * 7)
+        .toISOString()
+        .substring(0, 10),
+    ),
+    end: parseDate(
+      new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString().substring(0, 10),
+    ),
+  };
+
+  const last30Days = {
+    start: parseDate(
+      new Date(Date.now() - 1000 * 60 * 60 * 24 * 30)
+        .toISOString()
+        .substring(0, 10),
+    ),
+    end: parseDate(
+      new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString().substring(0, 10),
+    ),
+  };
+
   if (props.hidden) {
     return <></>;
   }
@@ -32,7 +66,23 @@ export const PositionDateRangePicker = (props: {
   return (
     <div className="mr-4 my-4 max-w-sm whitespace-nowrap col-span-2">
       <DateRangePicker
+        CalendarTopContent={
+          <ButtonGroup>
+            <Button color="primary" onPress={() => updateDates(today)}>
+              Today
+            </Button>
+            <Button color="primary" onPress={() => updateDates(last7Days)}>
+              Last 7 Days
+            </Button>
+            <Button color="primary" onPress={() => updateDates(last30Days)}>
+              Last 30 Days
+            </Button>
+          </ButtonGroup>
+        }
         aria-label="Transaction Date Range"
+        calendarProps={{
+          focusedValue: parseDate(props.start.toISOString().substring(0, 10)),
+        }}
         label="Transaction Date Range"
         value={dateRange}
         onChange={(range) => updateDates(range)}
