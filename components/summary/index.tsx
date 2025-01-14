18f6637abe5b0d0c97265be6b87ade0761d9bfac
GeekLad <geeklad@geeklad.com>
2025-01-14 09:13:40 -0500

    Updated extra div height to resolve base token dropdown issue

--- components/summary/index.tsx
+++ components/summary/index.tsx
@@ -205,9 +205,7 @@ export const Summary = (props: { downloadWorker: Worker }) => {
           />
         </div>
         {quoteTokenDisplay}
-        {filter?.baseTokenMints?.size && filter?.baseTokenMints?.size >= 10 ? (
-          <div className="min-h-1" />
-        ) : null}
+        <div className="min-h-10" />
       </div>
     </section>
   );
